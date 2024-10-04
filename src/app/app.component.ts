87eb5efa09b32053bc83f44ed084ecba2e5d68fe
alexandre cardoso <alexandre.cardoso@redes.unb.br>
2024-10-04 20:12:06 -0300

    Componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+//Components
+import { NewComponent } from './components/new-component/new-component.component';
+import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent],
+  template: `
+  <!-- <router-outlet></router-outlet> -->
+  <!-- <app-new-component/> -->
+  <h1>Curso de Angular</h1> 
+  <app-template-binding />    
+  `,
 })
-export class AppComponent {
-  title = 'meu-primeiro-projeto-latest';
-}
+export class AppComponent {}
+
+
---
+++ src/app/components/template/template-binding/template-binding.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-binding',
+  standalone: true,
+  imports: [],
+  templateUrl: './template-binding.component.html',
+  styleUrl: './template-binding.component.scss'
+})
+export class TemplateBindingComponent {
+  public name = 'Dener Troquatte';
+  public age = 32;
+
+  public  sum(val1: number, val2: number): number {
+    return val1 + val2;
+  }
+
+}
