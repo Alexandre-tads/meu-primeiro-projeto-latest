e4347b0d22849290228e6f9d843458d2e3e1be30
alexandre cardoso <alexandre.cardoso@redes.unb.br>
2024-10-07 19:03:56 -0300

    template-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,27 @@ import { RouterOutlet } from '@angular/router';
 //Components
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from "./components/template/template-variables/template-variables.component";
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NewComponent,
+    TemplateBindingComponent,
+    TemplateVariablesComponent,
+    TemplateControlFlowComponent
+  ],
   template: `
   <!-- <router-outlet></router-outlet> -->
   <!-- <app-new-component/> -->
-  <h1>Curso de Angular</h1> 
-  <app-template-binding />    
+  <h1>Curso de Angular</h1>
+  <app-template-control-flow />
+  
   `,
 })
 export class AppComponent {}
--- src/app/components/template/template-binding/template-binding.component.ts
+++ src/app/components/template/template-binding/template-binding.component.ts
@@ -1,18 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-binding',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './template-binding.component.html',
   styleUrl: './template-binding.component.scss'
 })
 export class TemplateBindingComponent {
   public name = 'Dener Troquatte';
   public age = 32;
+  public condition = this.age > 1 ? "Teste" : "Teste2";
+  public isDisabled = true;
+  public srcValue =
+  'https://vidafullstack.com.br/wp-content/uploads/2022/04/Aprenda-o-que-e-Databinding-com-Angular-1080x675.jpg';
 
-  public  sum(val1: number, val2: number): number {
-    return val1 + val2;
+  public isTextDecoration = this.age > 32 ? 'underline' : 'none';
+  
+
+  public  sum() {
+    return this.age++;
   }
 
+  public  sub() {
+    return this.age--;
+  }
+
+  public onKeyDown(event: Event) {
+    return console.log(event);
+  }
+
+  public onMouseMove(event:any) {
+    return console.log({
+      clientX: event.clientX,
+      clientY: event.clientY,
+    })
+  }
 }
